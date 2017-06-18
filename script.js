3b649c6491bc30852824a091240810990a1126c9
ollepeter <mr.peterolle@gmail.com>
2017-06-18 23:50:36 +0200

    add functionality for the operator keys

--- script.js
+++ script.js
@@ -1,50 +1,111 @@
 
 var inputNumber = "";
+var prevInputNumber = "";
+var prevInputOperator = "";
 var equation = "";
 var display = "";
+var basicOperators = "x-+÷%";
 var buttons = document.getElementsByTagName("button");
 for (var i = 0; i < buttons.length; i++) {
     buttons[i].onclick = calc;
 }
 
 function calc() {
-    equation = document.getElementById("display").innerHTML;
-    inputNumber = equation;
     var btnValue = this.innerHTML;
     switch (btnValue) {
         case "C":
+            inputNumber = "";
             equation = "";
-            inputNumber = "";   
-            display = equation;
+            display = "";
+            prevInputNumber = "";
+            prevInputOperator = "";
             break;
         case ".":
-            console.log(inputNumber.length)
+            if (inputNumber.length == 0) {
+                inputNumber += "0.";
+                equation += "0.";
+            }
             if (inputNumber.indexOf(".") == -1) {
-                equation += btnValue;
                 inputNumber += btnValue;
+                equation += btnValue;
             }
-            if (inputNumber.length == 1) {
-                equation = "0" + btnValue;
-                inputNumber = "0" + btnValue;
-            }
-            display = equation;
-            break; 
-        case "=":
-            equation += btnValue;
-            var finalEquation = equation.slice(0, -1);
-            result = eval(finalEquation);
-            display = result;
+            display += btnValue;
             break;
+        case "+":
+        case "-":
+            if (basicOperators.indexOf(display.slice(-1)) == -1) {
+                equation += btnValue;
+                display += btnValue;
+            }
+            prevInputNumber = inputNumber;
+            prevInputOperator = btnValue;
+            inputNumber = "";
+            break;
+        case "x":
+            if (basicOperators.indexOf(display.slice(-1)) == -1) {
+                equation += "*";
+                display += btnValue;
+            }
+            prevInputNumber = inputNumber;
+            prevInputOperator = btnValue;
+            inputNumber = "";
+            break;
+        case "÷":
+            if (basicOperators.indexOf(display.slice(-1)) == -1) {
+                equation += "/";
+                display += btnValue;
+            }
+            prevInputNumber = inputNumber;
+            prevInputOperator = btnValue;
+            inputNumber = "";
+            break;
+        case "%":
+            if (basicOperators.indexOf(display.slice(-1)) == -1) {
+                switch (prevInputOperator) {
+                    case "+":
+                        equation += "-" + inputNumber + "+(" + prevInputNumber + "/100*" + inputNumber + ")";
+                        break;
+                    case "-":
+                        equation += "+" + inputNumber + "-(" + prevInputNumber + "/100*" + inputNumber + ")";
+                        break;
+                    default:
+                        equation += "*0.01*"
+                    break;
+            }
+            display += btnValue;
+        }
+            inputNumber = "";
+            break;
+        case "=":
+            if (basicOperators.indexOf(display.slice(-1)) !== -1) {
+                equation = display.slice(0, -1)
+            }
+            try {
+                var result = eval(equation).toString();
+                inputNumber = result;
+                equation = result;
+                display = equation;
+            }
+            catch (SyntaxError) {
+                alert("The syntax of the equation is not correct!")
+            }
+            finally {
+                break;
+            }
         default:
             equation += btnValue;
             inputNumber += btnValue;
-            display = equation;
+            display += btnValue;
+            break;
     }
     document.getElementById("display").innerHTML = display;
 
+    /* The below lines are for testing.
     document.getElementById("btnValue").innerHTML = "btnValue: " + btnValue;
     document.getElementById("inputNumber").innerHTML = "inputNumber: " + inputNumber;
     document.getElementById("equation").innerHTML = "equation: " + equation;
-    document.getElementById("finalEquation").innerHTML = "finalEquation: " + finalEquation;
-    document.getElementById("result").innerHTML = "result: " + result;
+    document.getElementById("screen").innerHTML = "display: " + display;
+    document.getElementById("prevInputNumber").innerHTML = "prevInputNumber: " + prevInputNumber;
+    document.getElementById("prevInputOperator").innerHTML = "prevInputOperator: " + prevInputOperator;
+    */
 }
