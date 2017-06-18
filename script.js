317a3fae9a530b2333d4a0834e95884ea011788e
ollepeter <mr.peterolle@gmail.com>
2017-06-18 15:05:57 +0200

    add deciaml key (only one '.'allowed in a number)

--- script.js
+++ script.js
@@ -1,30 +1,50 @@
 
+var inputNumber = "";
+var equation = "";
+var display = "";
 var buttons = document.getElementsByTagName("button");
 for (var i = 0; i < buttons.length; i++) {
     buttons[i].onclick = calc;
 }
 
 function calc() {
+    equation = document.getElementById("display").innerHTML;
+    inputNumber = equation;
     var btnValue = this.innerHTML;
-    document.getElementById("display").innerHTML += btnValue;
-    var equation = document.getElementById("display").innerHTML;
     switch (btnValue) {
         case "C":
-            document.getElementById("display").innerHTML = "";
+            equation = "";
+            inputNumber = "";
+            display = equation;
             break;
         case ".":
-            break;           
+            console.log(inputNumber.length)
+            if (inputNumber.indexOf(".") == -1) {
+                equation += btnValue;
+                inputNumber += btnValue;
+            }
+            if (inputNumber.length == 1) {
+                equation = "0" + btnValue;
+                inputNumber = "0" + btnValue;
+            }
+            display = equation;
+            break;
                 
         case "=":
+            equation += btnValue;
             var finalEquation = equation.slice(0, -1);
             result = eval(finalEquation);
-            document.getElementById("display").innerHTML = result;
+            display = result;
             break;
+        default:
+            equation += btnValue;
+            inputNumber += btnValue;
+            display = equation;
     }
-
-    
+    document.getElementById("display").innerHTML = display;
 
     document.getElementById("btnValue").innerHTML = "btnValue: " + btnValue;
+    document.getElementById("inputNumber").innerHTML = "inputNumber: " + inputNumber;
     document.getElementById("equation").innerHTML = "equation: " + equation;
     document.getElementById("finalEquation").innerHTML = "finalEquation: " + finalEquation;
     document.getElementById("result").innerHTML = "result: " + result;
